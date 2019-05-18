4826c859241837f97c975091e193825d66bcbefc
DVitsme <derrick.valentine@gmail.com>
2019-05-17 21:34:41 -0400

    page generation fixed

---
+++ src/components/hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Welcome to my Blog</h1>
+      <p>
+        Welcome to my blog this will just be a random collection of thoughts.{' '}
+      </p>
+    </div>
+  );
+};
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
-import Layout from '../src/components/layout';
-import ReadLink from '../src/components/read-link';
+import { MDXRenderer } from 'gatsby-mdx';
+import Layout from '../components/layout';
+import ReadLink from '../components/read-link';
 
-const PostTemplate = ({ pageContext }) => (
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      code {
+        body
+      }
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => (
   <Layout>
-    <h1>post title</h1>
+    <h1>{post.frontmatter.title}</h1>
     <p
       css={css`
         font-size: 0.75rem;
       `}
     >
-      Post by
+      Post by {post.frontmatter.author}
     </p>
-    <p>Post body goes here</p>
+    <MDXRenderer>{post.code.body}</MDXRenderer>
     <ReadLink to="/">Back to all post &larr;</ReadLink>
   </Layout>
 );
