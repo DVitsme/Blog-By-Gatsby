6b5cfc4406479b08cf65e75f5b12c271932f8cee
DVitsme <derrick.valentine@gmail.com>
2019-05-20 23:45:01 -0400

    Hero Added fix file image file size

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,13 +2,53 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 
-const Hero = () => {
+const ImageBackground = styled('div')`
+  background-image: url('/images/Hero.jpg');
+  background-position: top 20% center;
+  background-size: cover;
+  height: 75vh;
+  + * {
+    margin-top: 0;
+  }
+`;
+const Textbox = styled('div')`
+  background-image: linear-gradient(to top, #00000077 2rem, #ddbbff00);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  padding: 0 calc((100vw - 1080px) / 2) 2rem;
+  width: 100%l;
+
+  h1 {
+    color: #fff;
+    text-shadow: 1px 1px 3px #eeddff66;
+    font-size: 2.25rem;
+  }
+
+  p,
+  a {
+    color: #fff;
+    margin-top: 8px;
+    font-size: 1.32rem;
+  }
+
+  a {
+    margin-top: 0.5rem;
+    font-size: 1rem;
+  }
+`;
+
+export default function Hero() {
   return (
-    <div>
-      <h1>Welcome to my Blog</h1>
-      <p>
-        Welcome to my blog this will just be a random collection of thoughts.{' '}
-      </p>
-    </div>
+    <ImageBackground>
+      <Textbox>
+        <h1>Welcome to my Blog</h1>
+        <p>
+          Welcome to my blog this will just be a random collection of thoughts.{' '}
+        </p>
+        <Link to="/about/">To About &rarr;</Link>
+      </Textbox>
+    </ImageBackground>
   );
-};
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
 
@@ -8,13 +9,14 @@ export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Hi im alive</h1>
-      <Link to="/about/">To About &rarr;</Link>
-      <h2>Blog Post</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <React.Fragment>
+      <Hero />
+      <Layout>
+        <h2>Blog Post</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </React.Fragment>
   );
 };
