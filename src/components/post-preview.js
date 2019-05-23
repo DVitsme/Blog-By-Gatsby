04936234b918f1181e0af73c5a2d1bdf613ea5f7
DVitsme <derrick.valentine@gmail.com>
2019-05-22 23:41:41 -0400

    Images optimized Remark setup

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
+import Image from 'gatsby-image';
 import ReadLink from './read-link';
 
 export default function PostPreview({ post }) {
@@ -10,17 +11,37 @@ export default function PostPreview({ post }) {
         border-bottom: 1px solid #ddd;
         margin-top: 0.75rem;
         padding-bottom: 1rem;
+        display: flex;
 
         :first-of-type {
           margin-top: 1rem;
         }
       `}
     >
-      <h3>
-        <Link to={post.slug}>{post.title}</Link>
-      </h3>
-      <p>{post.excerpt}</p>
-      <ReadLink to={post.slug}>Read More &rarr;</ReadLink>
+      <Link
+        to={post.slug}
+        css={css`
+          margin: 1rem 1rem 0 0;
+          width: 200px;
+        `}
+      >
+        <Image
+          fluid={post.image.sharp.fluid}
+          css={css`
+            * {
+              margin-top: 0;
+            }
+          `}
+          alt={post.title}
+        />
+      </Link>
+      <div>
+        <h3>
+          <Link to={post.slug}>{post.title}</Link>
+        </h3>
+        <p>{post.excerpt}</p>
+        <ReadLink to={post.slug}>Read More &rarr;</ReadLink>
+      </div>
     </article>
   );
 }
