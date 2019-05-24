8514710985f6fd0ebc2ffb840f7cfa70d2aae688
DVitsme <derrick.valentine@gmail.com>
2019-05-23 23:59:27 -0400

    insta in

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Hero from '../components/hero';
+import Insta from '../components/insta';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
 
@@ -16,6 +17,7 @@ export default () => {
         {posts.map(post => (
           <PostPreview key={post.slug} post={post} />
         ))}
+        <Insta />
       </Layout>
     </React.Fragment>
   );
